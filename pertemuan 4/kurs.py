7ba660cc27c7a583a3851711b81fed5cc1325768
MuhammadRezaAgusman <muhammadrezaagusman@gmail.com>
2026-02-27 15:28:41 +0700

    tugas praktikum 4 strukdat, aplikasi konversi matauang

--- pertemuan 4/kurs.py
+++ pertemuan 4/kurs.py
@@ -1,16 +1,16 @@
 kurs = {"USD":"16.875", 
         "EUR":"19.995", 
         "SGD":"13.360", 
-        "JPY":"109"} #DATA KURS
+        "JPY":"109"} #DATA KURS dalam string
 
 kurs_inInt = {"USD":16875, 
               "EUR":19995, 
               "SGD":13360, 
               "JPY":109,
-              "IDR":1}
+              "IDR":1}#data nilai kurs dalam int
 
 kode_mata_uang = {"USD":"USD", 
                   "EUR":"EUR", 
                   "SGD":"SGD", 
                   "JPY":"JPY",
-                  "IDR":"Rp"}
+                  "IDR":"Rp"}#kode mata uang
--- pertemuan 4/main.py
+++ pertemuan 4/main.py
@@ -3,7 +3,6 @@ import kurs as krs
 import konverter as kvt 
 
 print("=== KONVERTER MATA UANG ===")
-data = [[k, v] for k, v in krs.kurs.items()]
-print(tabulate.tabulate(data, headers=['Kode', 'Kurs'], tablefmt="pretty"))
+print(tabulate.tabulate(krs.kurs.items(), headers=['Kode', 'Kurs'], tablefmt="pretty"))
 
 kvt.convert()
